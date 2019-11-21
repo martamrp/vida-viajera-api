3dbec5103a79a30c7a3cb3bf7ae2cc226cbf800f
martamrp <martaromeroperez86@gmail.com>
2019-11-21 20:25:47 +0100

    Script que incluye en la tabla precio, pais_origen y pais destino

---
+++ database/04_alter_table_trip.sql
@@ -0,0 +1,3 @@
+ALTER TABLE vidaviajera.trip ADD precio int NOT NULL;
+ALTER TABLE vidaviajera.trip ADD pais_origen VARCHAR(2) NOT NULL;
+ALTER TABLE vidaviajera.trip ADD pais_destino VARCHAR(2) NOT NULL;
